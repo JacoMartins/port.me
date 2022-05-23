33f85ceb43650e40cd86746f0386ff2087dad609
JacoMartins <jaccobphotos271@gmail.com>
2022-05-23 04:57:15 -0300

    Meu deus, meu senhor.

--- server/prisma/migrations/20220523075637_eu_n_aguento_mais/migration.sql
+++ server/prisma/migrations/20220523075637_eu_n_aguento_mais/migration.sql
@@ -1,9 +1,18 @@
 -- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "display_name" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "profiles" (
     "username" TEXT NOT NULL PRIMARY KEY,
     "display_name" TEXT NOT NULL,
     "welcome_phrase" TEXT NOT NULL,
-    "description" TEXT NOT NULL
+    "description" TEXT NOT NULL,
+    "profile_picture" TEXT NOT NULL
 );
 
 -- CreateIndex
