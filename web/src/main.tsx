5a88e7b25b3415660e6dba858a4a8077c6d15c3c
Jacó Martins <58242482+JacoMartins@users.noreply.github.com>
2022-06-16 01:29:22 -0300

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App'
-import { Header } from './components/Header';
 import { AuthProvider } from './contexts/AuthContext';
 import { GlobalStyle } from './global';
 import CreateAccount from './routes/CreateAccount';
